34afe9c43c61890f2ac409e790d324bf6c217acf
racontre <7081938+racontre@users.noreply.github.com>
2023-07-09 11:39:30 +0300

    Use moscow_price for scraper

--- src/grabber/nsreg/spiders/nsreg_redreg.py
+++ src/grabber/nsreg/spiders/nsreg_redreg.py
@@ -1,7 +1,7 @@
 import scrapy
 from nsreg.items import NsregItem
 
-from ..utils import find_price
+from ..utils_spiders import moscow_price
 
 REGEX_PATTERN = r"([0-9]+[.,\s])?руб"
 EMPTY_PRICE = {
@@ -16,16 +16,6 @@ class NsregRedregSpider(scrapy.Spider):
     start_urls = ['https://www.red-reg.ru/price/']
 
     def parse(self, response):
-        pricereg = response.xpath('//article[@class="b-prices__item b-section"]/div/table/tr[5]/td[@class="b-table__cell"]/text()').get()
-        pricereg = find_price(REGEX_PATTERN, pricereg)
-        priceprolong = response.xpath('//article[@class="b-prices__item b-section"]/div/table/tr[5]/td[@class="b-table__cell b-table__cell_last"]/text()').get()
-        priceprolong = find_price(REGEX_PATTERN, priceprolong)
-
-        item = NsregItem()
-        item['name'] = "ООО «Редрег»"
-        price = item.get('price', EMPTY_PRICE)
-        price['pricereg'] = pricereg
-        price['priceprolong'] = priceprolong
-        item['price'] = price
+        item = moscow_price(self, response, REGEX_PATTERN, "ООО «Редрег»")
 
         yield item
