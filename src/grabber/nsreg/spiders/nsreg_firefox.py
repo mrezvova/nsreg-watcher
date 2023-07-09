e26204beeaa6b2e19fb34c1a1676f8fc8280611c
Rufina Talalaeva <rufusnorbert14@gmail.com>
2023-07-09 21:48:21 +0300

    fix: use existing function for parsing firefox

--- src/grabber/nsreg/spiders/nsreg_firefox.py
+++ src/grabber/nsreg/spiders/nsreg_firefox.py
@@ -1,20 +1,9 @@
-import logging
-import re
-
 import scrapy
-from nsreg.items import NsregItem
 
-from ..utils import find_price
+from ..utils_spider import moscow_tariffs
 
 REGEX_PATTERN = r"([0-9]+)"
-EMPTY_PRICE = {
-    'pricereg': None,
-    'priceprolong': None,
-    'pricechange': None,
-}
-
-def xpath(index: int) -> str:
-    return f'/html/body/section/div/div/div/div[@class="border rounded"]/div[{index}]/div[2]/span/text()'
+name = "ООО «ФАЕРФОКС»"
 
 
 class NsregFirefoxSpider(scrapy.Spider):
@@ -23,16 +12,5 @@ class NsregFirefoxSpider(scrapy.Spider):
     start_urls = ["https://firefox.ru/site/tariffs"]
 
     def parse(self, response):
-        pricereg = find_price(REGEX_PATTERN, response.xpath(xpath(1)).get())
-        priceprolong = find_price(REGEX_PATTERN, response.xpath(xpath(2)).get())
-        pricechange = find_price(REGEX_PATTERN, response.xpath(xpath(3)).get())
-
-        item = NsregItem()
-        item['name'] = "ООО «ФАЕРФОКС»"
-        price = item.get('price', EMPTY_PRICE)
-        price['pricereg'] = pricereg
-        price['priceprolong'] = priceprolong
-        price['pricechange'] = pricechange 
-        item['price'] = price
-
+        item = moscow_tariffs(self, response, REGEX_PATTERN, name)
         yield item
